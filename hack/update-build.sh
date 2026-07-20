f21aae07e8dad373d688c8fad1fd4b1395b0c270
Joel Takvorian <jtakvori@redhat.com>
2026-07-20 12:05:44 +0200

    fix bundles dir

--- hack/update-build.sh
+++ hack/update-build.sh
@@ -5,12 +5,11 @@
 echo "Updating container file for v${BUILDVERSION}"
 
 # supported_ocp_versions="v4.13"
-manifests_dir="./bundles/openshift/manifests"
-# metadata_dir="./bundle/metadata"
+bundle_dir="./bundles/openshift"
 crd_name="flows.netobserv.io_flowcollectors.yaml"
-crd_file="${manifests_dir}/${crd_name}"
+crd_file="${bundle_dir}/manifests/${crd_name}"
 csv_name="netobserv-operator.clusterserviceversion.yaml"
-csv_file="${manifests_dir}/${csv_name}"
+csv_file="${bundle_dir}/manifests/${csv_name}"
 
 source ./hack/nudging/container_digest.sh
 
@@ -22,8 +21,8 @@ export EPOC_TIMESTAMP=$(date +%s)
 
 VERSION="${BUILDVERSION}" TARGET_CSV_FILE="${csv_file}" python3 ./hack/patch_csv.py
 
-sed -i 's/operators.operatorframework.io.bundle.channels.v1: latest,community/operators.operatorframework.io.bundle.channels.v1: stable/g' ./bundle/metadata/annotations.yaml
-sed -i 's/operators.operatorframework.io.bundle.channel.default.v1: community/operators.operatorframework.io.bundle.channel.default.v1: stable/g' ./bundle/metadata/annotations.yaml
+sed -i 's/operators.operatorframework.io.bundle.channels.v1: latest,community/operators.operatorframework.io.bundle.channels.v1: stable/g' ${bundle_dir}/metadata/annotations.yaml
+sed -i 's/operators.operatorframework.io.bundle.channel.default.v1: community/operators.operatorframework.io.bundle.channel.default.v1: stable/g' ${bundle_dir}/metadata/annotations.yaml
 
 #Using downstream base image
 echo "Container file updated"
