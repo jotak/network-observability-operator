71abc1aa32455f71e8fabc236a6b2b1bd97b58d7
Joel Takvorian <jtakvori@redhat.com>
2026-09-10 14:53:03 +0200

    clusterroles typo/plural

--- internal/pkg/manager/roles.go
+++ internal/pkg/manager/roles.go
@@ -34,7 +34,7 @@ package manager
 // Operator needs to update specific cluster roles bindings for operands
 //+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterrolebindings,resourceNames=netobserv-loki-writer;netobserv-informers;netobserv-hostnetwork;netobserv-token-review;netobserv-flowcollector-viewer-role,verbs=get;update;watch
 // "Bind" allows to bind those exact CRBs without the operator needing the same rbac transitively
-//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterrole,verbs=bind,resourceNames=netobserv-loki-writer;netobserv-informers;netobserv-hostnetwork;netobserv-token-review;netobserv-flowcollector-viewer-role
+//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,verbs=bind,resourceNames=netobserv-loki-writer;netobserv-informers;netobserv-hostnetwork;netobserv-token-review;netobserv-flowcollector-viewer-role
 
 // Operator needs to patch Console CR (cluster scope)
 //+kubebuilder:rbac:groups=operator.openshift.io,resources=consoles,verbs=get;list;patch;watch
